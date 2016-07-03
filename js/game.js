89f587871779ccc1f377343c632c47a18d748a39
Minwoo Kim <mwkim0919@gmail.com>
2016-07-02 23:14:41 -0700

    added jquery scripts. clicking tiles works. started working on game menu.

--- js/game.js
+++ js/game.js
@@ -1,7 +1,23 @@
 var WIDTH = 500;
 var HEIGTH = 500;
 var num_array = [];
+var current_num = 1;
 var next_num = 26;
+var gameMenu = {
+	start: function() {
+		var button = '<div id="start-button" onclick="startGame()">' +
+			'Click to Start' + '</div>';
+		document.getElementById('game-container').innerHTML = button;
+	}
+}
+
+function countDown() {
+	var timer = duration;
+}
+
+function startGame() {
+	newBoard();
+}
 
 function init() {
 	for (var i = 1; i <= 25; i++) {
@@ -26,10 +42,30 @@ function newBoard() {
 	var output = '';
     // memory_array.memory_tile_shuffle();
 	for (var i = 0; i < 25; i++) {
-		output += '<div id="tile_'+i+'">' + num_array[i] +'</div>';
+		output += '<div id="tile" onclick="clickTile(this)">' + num_array[i] +'</div>';
 	}
 	document.getElementById('game-container').innerHTML = output;
 }
 
-newBoard();
+function clickTile(tile) {
+	if (tile.innerHTML == current_num) {
+		if (current_num <= 25) {
+			$(tile).fadeOut(160, function() {
+				$(tile).text(next_num).fadeIn(160);
+				current_num++;
+				next_num++;
+			});
+		} else {
+			$(tile).fadeOut(150, function() {
+				$(tile).text("").fadeIn(160);
+				current_num++;
+			});
+		}
+	} else {
+		// $(tile).effect("shake", {distance: 1});
+	}
+}
+
+gameMenu.start();
+// newBoard();
 
