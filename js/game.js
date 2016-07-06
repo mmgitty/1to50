1458499d34f80d321cdc4ac327b8a53c3fd9d05a
Minwoo Kim <mwkim0919@gmail.com>
2016-07-06 13:59:11 -0700

    media query added. fixed countdown -> new game problem. deleted unnecessary codes.

--- js/game.js
+++ js/game.js
@@ -2,6 +2,7 @@ var num_array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,
 var current_num = 1;
 var next_num = 26;
 var best_score =  Number.MAX_SAFE_INTEGER;
+var is_playing = false;
 
 // Game Start Menu
 var gameMenu = {
@@ -73,27 +74,39 @@ function clickTile(tile) {
 function gameReset() {
 	current_num = 1;
 	next_num = 26;
+	is_playing = false;
 }
 
 // starting game.
 function startGame() {
 	scoreReset();
+	is_playing = true;
 	var counter = 3;
 	var output = '<div id="timer">' + counter + '</div>';
 	$('.game-container').html(output);
 	var timer = setInterval(function() {
-		counter--;
-		if (counter <= 0) {
-			newBoard();
-			scoreStart();
-			clearInterval(timer);
+		if (is_playing) {
+			counter--;
+			if (counter <= 0) {
+				newBoard();
+				scoreStart();
+				clearInterval(timer);
+			} else {
+				$('#timer').html(counter);
+			}
 		} else {
-			$('#timer').html(counter);
+			// if a player clicks new game button during countdown,
+			// countdown timer has to be reset.
+			clearInterval(timer);
+			counter = 3;
+			return;
 		}
 	}, 1000);
 }
 
+// new game, goes back to game menu screen.
 function newGame() {
+	is_playing = false;
 	scoreStop();
 	scoreReset();
 	gameReset();
